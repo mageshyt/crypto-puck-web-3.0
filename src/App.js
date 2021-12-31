eaac4d49c2eff8f623c4538376cfd2d2576acfbf
magesh <hariarunengg@gmail.com>
2022-01-01 01:22:48 +0530

    working on puckcontainer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import Header from "./components/Header";
 import puckLogo from "./assets/header/cryptopunk-logo.png";
 import Collections from "./components/Collections";
+import PuckContainer from "./components/PuckContainer";
+
 function App() {
   return (
     <div className="bg-[#040303] p-[20px]  App">
       <Header puckLogo={puckLogo} />
+      <PuckContainer />
       <Collections />
     </div>
   );
--- src/components/PuckContainer.js
+++ src/components/PuckContainer.js
@@ -1,12 +1,22 @@
 import React from "react";
 
-const PuckContainer = () => {
+const PuckContainer = ({ selectedPuck }) => {
+  const image =
+    "https://lh3.googleusercontent.com/WrmpbMJMKmGfjJRCu2b0Bp_Pk2iPZcuRXyqPj7p3-I19DKHlIluigjdiwsnUZA28ssIQcydOnYgD2f6Qra05gFMZHWuojV1JSbM";
+  const name = "Gold Puck";
   return (
-    <div>
-      <div className="main-container">
-        <div className="puckHighlight">
+    <div className="main">
+      <div className="mainContent">
+        <div className="puckHighlighter">
           <div className="puckContainer">
-            <img src={"A"} className="" alt="" />
+            <img src={image} alt="selectedPuck" />
+          </div>
+        </div>
+        {/* Puck Details */}
+        <div className="puckDetails text-white">
+          {/* Title */}
+          <div className="title">
+            <span>{name}</span>
           </div>
         </div>
       </div>
