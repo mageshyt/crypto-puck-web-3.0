d0c34f4c9795f17e81b1be329e039ec8ce0f2e0d
magesh <hariarunengg@gmail.com>
2022-01-01 01:34:18 +0530

    icon added in puckcontainer

--- src/components/PuckContainer.js
+++ src/components/PuckContainer.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import InstaIcon from "../assets/owner/instagram.png";
+import TwitterIcon from "../assets/owner/twitter.png";
+import MoreIcon from "../assets/owner/more.png";
 const PuckContainer = ({ selectedPuck }) => {
   const image =
     "https://lh3.googleusercontent.com/WrmpbMJMKmGfjJRCu2b0Bp_Pk2iPZcuRXyqPj7p3-I19DKHlIluigjdiwsnUZA28ssIQcydOnYgD2f6Qra05gFMZHWuojV1JSbM";
@@ -15,10 +17,37 @@ const PuckContainer = ({ selectedPuck }) => {
         {/* Puck Details */}
         <div className="puckDetails text-white">
           {/* Title */}
-          <div className="title">
-            <span>{name}</span>
+          <div className="title">{name}</div>
+          {/* id */}
+          <span>•#23</span>
+
+          <div className="ownerDetails">
+            {/* image */}
+            <div>
+              <img src={image} className="h-12 w-12 " alt="ownerImg" />
+              {/* owner details */}
+              <div className="ownerDetails">
+                <div className="ownerhandel">
+                  <div>00943589012348902584</div>
+                  <div>@Magesh</div>
+                </div>
+                {/* insta youtube twitter icon */}
+                <div className="instaIcon">
+                  <img src={InstaIcon} alt="insta" />
+                </div>
+                {/* Twitter Icon */}
+                <div className="twitterIcon">
+                  <img src={TwitterIcon} alt="twitter" />
+                </div>
+                {/* more Icon */}
+                <div className="moreIcon">
+                  <img src={MoreIcon} alt="more" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        {/* Owner Details */}
       </div>
     </div>
   );
